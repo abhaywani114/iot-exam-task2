627218a501220b4ebd2120f2816adff1f304f2df
Abrar Ajaz <abhaywani114@gmail.com>
2024-03-30 01:21:43 +0530

    IOT

---
+++ database/migrations/2024_03_29_193618_create_iot_data_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('iot_data', function (Blueprint $table) {
+            $table->id();
+            $table->string('device_id');
+            $table->timestamp('timestamp');
+            $table->text('data');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('iot_data');
+    }
+};
