a12aaf412623c036b88ac8fc2d8d9343a9480350
Abrar Ajaz <abhaywani114@gmail.com>
2024-03-30 14:42:19 +0530

    visualization clean up

--- app/Http/Controllers/ITODataController.php
+++ app/Http/Controllers/ITODataController.php
@@ -22,30 +22,21 @@ class ITODataController extends Controller
                         ->orderBy('timestamp')
                         ->get();
 
-        // // Format data for Chart.js
-        // $labels = $data->pluck('formatted_timestamp'); // Assuming formatted timestamp column
-        // $values = $data->pluck('value_column'); // Replace 'value_column' with your actual data column
-
-        // // Return data as JSON
-        // return response()->json([
-        //     'labels' => $labels,
-        //     'values' => $values,
-        // ]);
         $data = IotData::whereBetween('timestamp', [$startDate, $endDate])
         ->select('device_id', DB::raw('COUNT(*) as packet_count'))
         ->groupBy('device_id')
         ->orderBy('device_id')
         ->get();
 
-// Format data for Chart.js
-$labels = $data->pluck('device_id');
-$values = $data->pluck('packet_count');
+        // Format data for Chart.js
+        $labels = $data->pluck('device_id');
+        $values = $data->pluck('packet_count');
 
-// Return data as JSON
-return response()->json([
-'labels' => $labels,
-'values' => $values,
-]);
+        // Return data as JSON
+        return response()->json([
+        'labels' => $labels,
+        'values' => $values,
+        ]);
 
     }
 
